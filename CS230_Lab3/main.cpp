96913e17c91cd81ac7d79442c9f382efa3356f65
Hasti Salsali <hastisalsali@gmail.com>
2025-04-25 17:07:34 -0700

    Initial Commit

---
+++ CS230_Lab3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CS230_Lab3
+//
+//  Created by Hasti Salsali on 4/25/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
