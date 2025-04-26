8d01a9be0bec186b75e969440f630d9c1e87ed7c
Hasti Salsali <hastisalsali@gmail.com>
2025-04-25 19:42:29 -0700

    writing memtocache

--- CS230_Lab3/main.cpp
+++ CS230_Lab3/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 const int BitsInMem = 32768 , BitsInCache = 2048 , CacheLineSize = 16, LinesInCache = 2048 / (2 * 16);
 struct CacheSlots {
-    int val = 0;
+    int val[4] = {};
     int tag;
     bool dirtyBit = 0;
     bool validBit = 0;
@@ -21,7 +21,9 @@ int GetMemVal (int address, int memory[]);
 int GetLine (int address);
 int GetTag (int address);
 int GetWay (CacheSlots cache[][2], int address);
-void WriteToCache (CacheSlots cache[][2], int address, int val);
+void WriteToCache (CacheSlots cache[][2], int memory[], int address, int val);
+void CacheToMem (CacheSlots cache[][2], int  memory[],  int line, int way);
+void MemToCache (CacheSlots cache[][2], int  memory[], int address, int way);
 //-----------------------------------------------------------------
 int main () {
     srand(time(0));
@@ -54,19 +56,50 @@ int GetWay (CacheSlots cache[][2], int address){
     return way;
 };
 
-void WriteToCache (CacheSlots cache[][2], int address, int val){
-    int way = GetWay(cache, address), line = GetLine(address), tag = GetTag(address);
+void WriteToCache (CacheSlots cache[][2], int memory[], int address, int val){
+    int way = GetWay(cache, address), line = GetLine(address), tag = GetTag(address), offset = address % 16;
     
     if (way == 3){
         way = rand() % 2;
-        //write cache to mem
+        MemToCache(cache, memory, address, way);
     }
-    cache[line][way].val = val;
+    cache[line][way].val[offset] = val;
     cache[line][way].tag = tag;
     cache[line][way].dirtyBit = true;
     cache[line][way].validBit = true;
 };
 
-void CacheToMem (CacheSlots cache[][2], int address){
-    int way = GetWay(cache, address), line = GetLine(address), tag = GetTag(address);
+void CacheToMem (CacheSlots cache[][2], int  memory[],  int line, int way){
+    int address, memIndex;
+    address = (cache[line][way].tag << 10) + (line << 4);
+    memIndex = address >> 2;
+    
+    if (cache[line][way].dirtyBit == true){
+        memory[memIndex] = cache[line][way].val[0];
+        memory[memIndex + 1] = cache[line][way].val[1];
+        memory[memIndex + 2] = cache[line][way].val[2];
+        memory[memIndex + 3] = cache[line][way].val[3];
+    }
+    cache[line][way].dirtyBit = false;
+}
+
+void MemToCache (CacheSlots cache[][2], int  memory[], int address, int way){
+    int line = GetLine(address), memIndex = address >> 2;
+    
+    
+    if (way == 3){
+        way = rand() % 2;
+    }
+    if (cache[line][way].dirtyBit == true){
+        CacheToMem(cache, memory, line, way);
+    }
+    cache[line][way].val[0] = memory[memIndex];
+    cache[line][way].val[1] = memory[memIndex + 1];
+    cache[line][way].val[2] = memory[memIndex + 2];
+    cache[line][way].val[3] = memory[memIndex + 3];
+    
+    cache[line][way].tag = GetTag(address);
+    cache[line][way].dirtyBit = false;
+    cache[line][way].validBit = true;
+    
 }
